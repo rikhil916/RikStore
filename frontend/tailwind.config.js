9c339b10690dc7214eb0947b03111f4b35a5616a
Chetan Rikhil <chetanrikhil916@gmail.com>
2025-03-15 18:37:18 +0530

    resolved tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,8 @@
 import flowbitePlugin from "flowbite/plugin";
+import tailwindcss from '@tailwindcss/vite'
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Ensure your paths are correct
+    content: ["./index.html", "./src/*/.{js,ts,jsx,tsx}"], // Ensure your paths are correct
     theme: {
     extend: {},
     },
